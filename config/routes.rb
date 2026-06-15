25f1b554790bcbd8bf9660c35dba538d05d625f6
Shiori Kido <kido@ShiorinoMacBook-Air.local>
2026-06-15 21:03:28 +0900

    ログイン・ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users, only: %i[new create]
 
+  get 'login', to: 'user_sessions#new', as: :login
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
+
   get "static_pages/top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
