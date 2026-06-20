7e1fd1567fc1772341438c6aa41e60210bcbf329
Shiori Kido <kido@ShiorinoMacBook-Air.local>
2026-06-20 10:51:49 +0900

    アカウント登録時のエラーメッセージを表示

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ end
 gem "aws-sdk-s3", "~> 1.226"
 
 gem "tailwindcss-rails", "~> 4.6"
+
+gem 'rails-i18n'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user) 
       redirect_to root_path, notice: 'ユーザー登録が完了しました'
     else
-      flash.now[:alert] = 'ユーザー登録に失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
